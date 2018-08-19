4e105b5a33471ad75ff5b2933b9f72e0a12ce5f2
Administrator <frank64168@gmail.com>
2018-08-19 14:59:20 +0800

    salary 員工查詢

---
+++ eform/Models/salary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using eform.Attributes;
+
+namespace eform.Models
+{
+    public class salary
+    {
+        [Key]
+        public string id { get; set; }
+        public int year { get; set; }
+        public int month { get; set;}
+        public string workNo { get; set; }
+        public string jData { get; set; }
+    }
+}
