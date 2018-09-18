3fda696eed502e8aa0baa0b762b47a1f0b5aa2ec
Administrator <frank64168@gmail.com>
2018-09-19 07:05:45 +0800

    add prj layout,navmenu

---
+++ eform/Controllers/PrjController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eform.Controllers
+{
+    public class PrjController : Controller
+    {
+        // GET: Prj
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
